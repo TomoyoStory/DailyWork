e1fc6b0dca976b2b860b7fe97eaa6c566fb6cdc7
LiMengZhen <tomoyo_story@qq.com>
2021-11-29 09:18:31 +0800

    修改BatchRename.py的接口为函数定义，这样便于之后引入模块函数进行直接调用。

--- Utils/ScriptTool/BatchRename.py
+++ Utils/ScriptTool/BatchRename.py
@@ -7,6 +7,15 @@ from tqdm import tqdm
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
+def batch_rename(path, start_pos):
+    for file in tqdm(os.listdir(path), desc='Changing the name'):
+        current_file_name = os.path.join(path, file)
+        if os.path.isfile(current_file_name):
+            new_name = os.path.join(path, '%07d.jpg'%start_pos) #! 这里可以修改整个索引的起始位置
+            os.rename(current_file_name, new_name)
+            start_pos= start_pos + 1
+    logging.info('All Finish! (*╹▽╹*),HaHa~')
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Change all the names of the files in dir to fix format!", epilog="It is easy! O(∩_∩)O~")
     parser.add_argument('-p', '--path', type=str, default='./', required=True, help='the file path to change name!', metavar='file_path')
@@ -14,12 +23,4 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--start_pos', type=int, default=0, help='the start index to increase', metavar='start_index')
     opt = parser.parse_args()
 
-    start_pos = opt.start_pos # start index
-    for file in tqdm(os.listdir(opt.path), desc='Changing the name'):
-        current_file_name = os.path.join(opt.path, file)
-        if os.path.isfile(current_file_name):
-            new_name = os.path.join(opt.path, '%07d.jpg'%start_pos) #! 这里可以修改整个索引的起始位置
-            os.rename(current_file_name, new_name)
-            start_pos= start_pos + 1
-    logging.info('All Finish! (*╹▽╹*),HaHa~')
-
+    batch_rename(opt.path, opt.start_pos)
